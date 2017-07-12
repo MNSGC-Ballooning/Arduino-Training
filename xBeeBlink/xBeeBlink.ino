abf0fc778c97524893ae2ccd39522c25e3504cb6
Ryan Bowers <rbowers314@gmail.com>
2017-07-12 10:16:47 -0600

    Added comments, and function to correctly return led time on

--- xBeeBlink/xBeeBlink.ino
+++ xBeeBlink/xBeeBlink.ino
@@ -7,8 +7,8 @@
 
 #define led 13
 
-SoftwareSerial xBee = SoftwareSerial(2,3);
-unsigned int numberOn = 0;
+SoftwareSerial xBee = SoftwareSerial(2,3);  //Pins to communicate with xBee
+unsigned int numberOn = 0;  //variables to track system activity
 unsigned long timeOn = 0, turnedOn;
 bool isOn = false;
 
@@ -18,27 +18,28 @@ void setup() {
 }
 
 void loop() {
-  String command = "";
+  String command = "";            //start with an empty string
   while (xBee.available() > 0) {
-    command += char(xBee.read());
-    delay(10);
+    command += char(xBee.read()); //add to it char by char when receiving xBee transmissions
+    delay(10);                    //wait briefly for more data to arrive before checking
   }
-  if (command.equals("FLIP")) {
+  if (command.equals("FLIP")) {   //toggle command
     if (isOn) ledOff();
     else ledOn();
   }
-  else if ((command.substring(0,2)).equals("BL")) {
-    byte times = (command.substring(2, command.length())).toInt();
+  else if ((command.substring(0,2)).equals("BL")) { //blink command
+    byte times = (command.substring(2, command.length())).toInt();  //check to see how many times to blink
     ledBlink(times);
   }
-  else if (command.equals("TIME"))
-    xBee.println("\nTime on (s): " + String(timeOn/1000.0, 3));
-  else if (command.equals("NUM"))
+  else if (command.equals("TIME"))  //report total time on command
+    xBee.println("\nTime on (s): " + String(getTimeOn(), 3));
+  else if (command.equals("NUM"))   //report times turned on command
     xBee.println("\nTurned on " + String(numberOn) + " times.");
-  else if (!command.equals(""))
+  else if (!command.equals(""))     //if a command was received, but not one of the above, display an error
     xBee.println("\nError - " + command + ": Command not recognized");
 }
 
+//turns led on and saves time and number
 void ledOn() {
   if (!isOn) {
     digitalWrite(led, HIGH);
@@ -48,6 +49,7 @@ void ledOn() {
   }
 }
 
+//turns led off and adds time to total
 void ledOff() {
   if (isOn) {
     digitalWrite(led, LOW);
@@ -56,6 +58,7 @@ void ledOff() {
   }
 }
 
+//blinks led a specified number of times
 void ledBlink(byte times) {
   for (; times > 0; times--) {
     ledOn();
@@ -65,4 +68,9 @@ void ledBlink(byte times) {
   }
 }
 
+//returns time LED has been on, in seconds
+float getTimeOn() {
+  if (!isOn) return timeOn/1000.0;
+  else return (timeOn + millis() - turnedOn)/1000.0;
+}
 
