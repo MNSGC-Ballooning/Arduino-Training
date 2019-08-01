309d90836c7ef82c629c4765a68bb721634611de
Billy Straub <46384464+strau257@users.noreply.github.com>
2019-08-01 16:44:05 -0500

    Update DigitalThermocouple.ino

--- DigitalThermocouple/DigitalThermocouple.ino
+++ DigitalThermocouple/DigitalThermocouple.ino
@@ -7,6 +7,7 @@
      Component                    | Pins used               | Notes
      -                              -                         -
      Adafruit Thermocouple #1     | D13 D12 D11 D10         | SCK, SDO, SDI, CS pins in that order
+     Adafruit Thermocouple #2     | D13 D12 D11 D9          | SCK, SDO, SDI, CS pins in that order
    -------------------------------------------------------------------------------------------------------------------------
 
 
@@ -17,21 +18,22 @@ The cs pins, on the other hand, have to be given their own individual digital pi
 For example, if there were two thermocouples, their SCK pins could be wired together and both run to D13.
 Also, their SDO pins could be wired together and both run to D12.
 Additionally, their SDI pins could be wired together and both run to D11.
-On the other hand, the cs (chip select) pins require their own digital pins, so thermocouple one might have cs run to D10.
-Since thermocouple two must use a different cs pin, it could run to D9.
+On the other hand, the cs (chip select) pins require their own digital pins, so thermocouple #1 might have cs run to D10.
+Since thermocouple #2 must use a different cs pin, it could run to D9.
+
+This wiring setup is known as an SPI (Serial Peripheral Interface) connection and is required by specific devices, such as this Adafruit thermocouple.
 */
 
 
 #include <Adafruit_MAX31856.h>  //Adafruit thermocouple library
 
 
-// For the use of software SPI, uncomment the following line.
+//For the use of software SPI, uncomment the following line.
 //Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10, 11, 12, 13);
 
 //For the use of hardware SPI, uncomment the following line.
-  Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10);
-
-//For the most part, it doesn't matter if the software SPI or the hardware SPI variation is used.
+Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(10);
+Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(9);
 
 
 
@@ -39,8 +41,11 @@ void setup() {
   
   Serial.begin(9600);                                 //Starts the serial monitor
 
-  maxthermo.begin();                                  //Starts the thermocouple
-  maxthermo.setThermocoupleType(MAX31856_TCTYPE_K);   //Sets the thermocouple to be type K from the library
+  maxthermo1.begin();                                 //Starts the thermocouple
+  maxthermo2.begin();
+  maxthermo1.setThermocoupleType(MAX31856_TCTYPE_K);  //Sets the thermocouple to be type K from the library
+  maxthermo2.setThermocoupleType(MAX31856_TCTYPE_K);
+      
   Serial.print("Thermocouple type K: ");              //Simply prints the words in quotation marks when started up
 
   delay(2000);                                        //Waits two seconds before the loop starts
@@ -51,8 +56,10 @@ void setup() {
 
 void loop() {
   
-  Serial.print("Thermocouple Temp: ");                         //Simply prints the words in quotation marks before listing the follow temperature from the line below.
-  Serial.println(maxthermo.readThermocoupleTemperature());     //Prints the thermocouple temperature
+  Serial.print("Thermocouple #1 Temp: ");                         //Simply prints the words in quotation marks before listing the follow temperature from the line below.
+  Serial.println(maxthermo1.readThermocoupleTemperature());     //Prints the thermocouple temperature
+  Serial.print("Thermocouple #2 Temp: ");
+  Serial.println(maxthermo2.readThermocoupleTemperature());
 
   delay(1000);                                                 //Waits one second before restarting the loop
   
